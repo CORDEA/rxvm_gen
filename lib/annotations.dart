fb9aa36b206308dfd8d2fe334e550d55d2bd507e
Yoshihiro Tanaka <contact@cordea.jp>
2021-06-05 07:04:20 +0900

    add annotation

---
+++ lib/annotations.dart
@@ -0,0 +1,3 @@
+class RxViewModel {
+  const RxViewModel();
+}
--- lib/src/builder.dart
+++ lib/src/builder.dart
@@ -1,5 +1,18 @@
-
+import 'package:analyzer/dart/element/element.dart';
 import 'package:build/build.dart';
+import 'package:rxvm_gen/annotations.dart';
 import 'package:source_gen/source_gen.dart';
 
-Builder rxVmBuilder(BuilderOptions options) => SharedPartBuilder([], 'rxvm');
+Builder rxVmBuilder(BuilderOptions options) =>
+    SharedPartBuilder([RxVmGenerator()], 'rxvm');
+
+class RxVmGenerator extends GeneratorForAnnotation<RxViewModel> {
+  @override
+  generateForAnnotatedElement(
+    Element element,
+    ConstantReader annotation,
+    BuildStep buildStep,
+  ) {
+    return 'void dispose() {}';
+  }
+}
